bc5a9880175da55ce5ec35859228a2e0ddf866f9
HenryLoenwind <henry@loenwind.info>
2016-01-06 22:27:49 +0100

    Added Silent's Gems Torch Bandolier

--- src/main/java/info/loenwind/middletorch/KeyInputHandler.java
+++ src/main/java/info/loenwind/middletorch/KeyInputHandler.java
@@ -1,16 +1,14 @@
 package info.loenwind.middletorch;
 
-import org.lwjgl.input.Keyboard;
-
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockTorch;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
+
+import org.lwjgl.input.Keyboard;
+
 import cpw.mods.fml.client.registry.ClientRegistry;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
@@ -21,38 +19,42 @@ import cpw.mods.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public class KeyInputHandler {
 
-	private static KeyInputHandler me;
-	private static KeyBinding keyBinding;
-	
-	public static void init() {
-		keyBinding = new KeyBinding("key.placetorch", Keyboard.CHAR_NONE, "key.categories.gameplay");
-		me = new KeyInputHandler();
-        ClientRegistry.registerKeyBinding(keyBinding);
-        FMLCommonHandler.instance().bus().register(me);
-	}
+  private static KeyInputHandler me;
+  private static KeyBinding keyBinding;
 
-	@SubscribeEvent
-    public void onKeyInput(InputEvent.MouseInputEvent event) {
-        if(keyBinding.isPressed()) {
-    		Minecraft mc = Minecraft.getMinecraft();
-    		int currentItem = mc.thePlayer.inventory.currentItem;
-    		for (int slot = 0; slot <= 8; slot++) {
-    			if (mc.thePlayer.inventory.mainInventory[slot] != null && mc.thePlayer.inventory.mainInventory[slot].getItem() instanceof ItemBlock) {
-    				Block block = ((ItemBlock) mc.thePlayer.inventory.mainInventory[slot].getItem()).field_150939_a;
-					if (block instanceof BlockTorch || block.getUnlocalizedName().equals("tile.decoration.stonetorch")) {
-	    				mc.thePlayer.inventory.currentItem = slot;
-	    				mc.playerController.onPlayerRightClick(
-	    						mc.thePlayer, mc.theWorld,
-	    						mc.thePlayer.inventory.mainInventory[slot],
-	    						mc.objectMouseOver.blockX, mc.objectMouseOver.blockY,
-	    						mc.objectMouseOver.blockZ, mc.objectMouseOver.sideHit,
-	    						mc.objectMouseOver.hitVec);
-	    				mc.thePlayer.inventory.currentItem = currentItem;
-	    				return;
-        			}
-    			}
-    		}
+  public static void init() {
+    keyBinding = new KeyBinding("key.placetorch", Keyboard.CHAR_NONE, "key.categories.gameplay");
+    me = new KeyInputHandler();
+    ClientRegistry.registerKeyBinding(keyBinding);
+    FMLCommonHandler.instance().bus().register(me);
+  }
+
+  @SubscribeEvent
+  public void onKeyInput(InputEvent.MouseInputEvent event) {
+    if (keyBinding.isPressed()) {
+      Minecraft mc = Minecraft.getMinecraft();
+      int currentItem = mc.thePlayer.inventory.currentItem;
+      for (int slot = 0; slot <= 8; slot++) {
+        if (isTorchItem(mc.thePlayer.inventory.mainInventory[slot])) {
+          mc.thePlayer.inventory.currentItem = slot;
+          mc.playerController.onPlayerRightClick(mc.thePlayer, mc.theWorld, mc.thePlayer.inventory.mainInventory[slot], mc.objectMouseOver.blockX,
+              mc.objectMouseOver.blockY, mc.objectMouseOver.blockZ, mc.objectMouseOver.sideHit, mc.objectMouseOver.hitVec);
+          mc.thePlayer.inventory.currentItem = currentItem;
+          return;
         }
+      }
     }
+  }
+
+  private boolean isTorchItem(ItemStack candidate) {
+    if (candidate == null || candidate.getItem() == null || candidate.stackSize <= 0) {
+      return false;
+    }
+    if (candidate.getItem() instanceof ItemBlock) {
+      Block block = ((ItemBlock) candidate.getItem()).field_150939_a;
+      return block instanceof BlockTorch || "tile.decoration.stonetorch".equals(block.getUnlocalizedName());
+    }
+    return "item.silentgems:TorchBandolier".equals(candidate.getItem().getUnlocalizedName(candidate));
+  }
 
 }
