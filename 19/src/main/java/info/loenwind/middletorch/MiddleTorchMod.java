d104cf73831be659c19f57c4297adddb73407294
Henry Loenwind <henry@loenwind.info>
2016-03-19 01:27:17 +0100

    Ported to 1.9

---
+++ 19/src/main/java/info/loenwind/middletorch/MiddleTorchMod.java
@@ -0,0 +1,29 @@
+package info.loenwind.middletorch;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = MiddleTorchMod.MODID, version = MiddleTorchMod.VERSION)
+public class MiddleTorchMod
+{
+    public static final String MODID = "middletorch";
+    public static final String VERSION = "3.0.3";
+    
+    @SidedProxy(clientSide="info.loenwind.middletorch.ClientProxy")
+    public static IProxy proxy;
+    
+    @EventHandler
+    public void init(FMLPreInitializationEvent event)
+    {
+		proxy.preinit(event);
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		proxy.init(event);
+    }
+}
