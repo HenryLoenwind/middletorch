89d9dfeb79a5339db085a6f6110305d9c3ad60f1
Unknown <henry@loenwind.info>
2017-12-28 09:26:08 +0100

    Made client side only

--- 1112/src/main/java/info/loenwind/middletorch/MiddleTorchMod.java
+++ 1112/src/main/java/info/loenwind/middletorch/MiddleTorchMod.java
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = MiddleTorchMod.MODID, version = MiddleTorchMod.VERSION)
+@Mod(modid = MiddleTorchMod.MODID, version = MiddleTorchMod.VERSION, clientSideOnly = true)
 public class MiddleTorchMod {
   public static final String MODID = "middletorch";
   public static final String VERSION = "5.0.6";
