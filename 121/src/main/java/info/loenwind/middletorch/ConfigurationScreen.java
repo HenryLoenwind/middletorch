87f8e70c271592eb34de26f58cfafcd38249b9e4
Henry Loenwind <henry@loenwind.info>
2024-07-31 18:03:45 +0200

    Upgraded neo

--- 121/src/main/java/info/loenwind/middletorch/ConfigurationScreen.java
+++ 121/src/main/java/info/loenwind/middletorch/ConfigurationScreen.java
@@ -9,36 +9,38 @@ import net.minecraft.client.gui.screens.options.controls.KeyBindsList.KeyEntry;
 import net.minecraft.client.gui.screens.options.controls.KeyBindsScreen;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
+import net.neoforged.fml.ModContainer;
 
 public class ConfigurationScreen extends OptionsSubScreen {
 
-	public ConfigurationScreen(Minecraft mc, Screen parent) {
-		super(parent, mc.options, Component.translatable(MiddleTorchMod.MODID + ".configuration.title"));
+	@SuppressWarnings("resource")
+	public ConfigurationScreen(final ModContainer container, final Screen parent) {
+		super(parent, Minecraft.getInstance().options, Component.translatable(MiddleTorchMod.MODID + ".configuration.title"));
 	}
 
 	@Override
 	protected void addOptions() {
-		list.addSmall(Button.builder(Component.translatable("controls.keybinds"), p_346358_ -> this.minecraft.setScreen(new InjectKeybindScreen(this, this.options))).build(), null);
+		list.addSmall(Button.builder(Component.translatable("controls.keybinds"), p_346358_ -> minecraft.setScreen(new InjectKeybindScreen(this, options))).build(), null);
 	}
 
 	private static class InjectKeybindScreen extends KeyBindsScreen {
 
-		public InjectKeybindScreen(Screen parent, Options options) {
+		public InjectKeybindScreen(final Screen parent, final Options options) {
 			super(parent, options);
 		}
-		
+
 		@Override
 		protected void addContents() {
 			super.addContents();
 			keyBindsList.children().removeAll(keyBindsList.children().stream().filter( entry -> !(entry instanceof KeyEntry) || (((KeyEntry) entry).key != KeyInputHandler.keyBinding)).toList());
 		}
-		
-	    @Override
-	    protected void addFooter() {
-	        resetButton = Button.builder(Component.translatable("controls.resetAll"), button -> {}).build();
-	        layout.addToFooter(Button.builder(CommonComponents.GUI_DONE, button -> onClose()).width(200).build());
-	    }
-		
+
+		@Override
+		protected void addFooter() {
+			resetButton = Button.builder(Component.translatable("controls.resetAll"), button -> {}).build();
+			layout.addToFooter(Button.builder(CommonComponents.GUI_DONE, button -> onClose()).width(200).build());
+		}
+
 	}
-	
+
 }
